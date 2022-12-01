ec7440ea80c128822cf87a551b3c52debac17bed
dkaodkaork <dkaodkaork@gmail.com>
2022-12-01 11:01:20 +0900

    k

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, body: "리액트 어려워요" },
+    { id: 2, body: "리액트 질문있어요" },
+  ]);
+
+  const [body, setBody] = useState("");
+
+  const addTodoHandler = () => {
+    const newTodo = {
+      id: todos.length + 1,
+      body: body,
+    };
+    if (body !== "") {
+      setTodos([...todos, newTodo]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <div className="inputbox-style">
+        <input
+          value={body}
+          className="input-style"
+          placeholder="내용을 입력하세요"
+          onChange={(e) => setBody(e.target.value)}
+        ></input>
+        <button onClick={addTodoHandler}>추가하기</button>
+      </div>
+
+      <div className="title-style">
+        <h1>Todo List</h1>
+      </div>
+      <div className="cardbox-style">
+        {todos.map((todo) => {
+          return (
+            <div className="card-style" todo={todo} key={todo.id}>
+              {todo.body}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
